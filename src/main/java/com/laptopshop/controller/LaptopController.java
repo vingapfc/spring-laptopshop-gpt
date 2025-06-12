5c1bd83beafa60aa509414935e234420b041371f
vingapfc <vingapfc@gmail.com>
2025-06-12 20:31:13 +0700

    Chuc nang them laptop

--- src/main/java/com/laptopshop/controller/LaptopController.java
+++ src/main/java/com/laptopshop/controller/LaptopController.java
@@ -3,8 +3,10 @@ package com.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.laptopshop.repository.LaptopRepository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LaptopController {
@@ -19,4 +21,17 @@ public class LaptopController {
         model.addAttribute("laptops", repository.findAll());
         return "laptops";
     }
+
+    @GetMapping("/laptops/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("laptop", new com.laptopshop.model.Laptop());
+        return "laptop-form";
+    }
+
+    @PostMapping("laptops/save")
+    public String saveLaptop(@ModelAttribute("laptop") com.laptopshop.model.Laptop laptop) {
+        repository.save(laptop);
+        return "redirect:/laptops";
+    }
+
 }
